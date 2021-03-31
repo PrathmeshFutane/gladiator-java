ee57363c1ac7ad98a6010d00b5d487977cbc9f5e
shreyash304 <2013shreyash@gmail.com>
2021-03-31 21:05:25 +0530

    this edit done by shreyash

--- hibernate-demo3-my/src/main/java/com/lti/app/App.java
+++ hibernate-demo3-my/src/main/java/com/lti/app/App.java
@@ -3,5 +3,6 @@ package com.lti.app;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("this is first java project on github");
+		System.out.println("this is second edit done by shreyash");
 	}
 }
