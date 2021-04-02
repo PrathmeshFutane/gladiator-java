c986a9e0577f017dc7956d424462c32f26b97ed9
jeshma <jeshma.m26@gmail.com>
2021-04-02 10:41:43 +0530

    third commit

--- hibernate-demo3-my/src/main/java/com/lti/app/App.java
+++ hibernate-demo3-my/src/main/java/com/lti/app/App.java
@@ -4,5 +4,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("this is first java project on github");
 		System.out.println("this is second edit done by shreyash");
+		System.out.println("this is third edit done by Jeshma");
 	}
 }
